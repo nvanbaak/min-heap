d63b493595c7ac4d287dbc4b0098c430b1982b0e
Nik Van Baak <nvanbaak@gmail.com>
2021-12-01 13:23:23 -0800

    formatted basic commands and implemented basic functionality

--- MinHeap.py
+++ MinHeap.py
@@ -1,23 +1,55 @@
 class MinHeap():
+    """
+    A node-based implementation of a MinHeap.
+    """
+    class HeapNode():
+        """
+        Nodes store one datum and links to other nodes in the heap.
+        """
+        def __init__(self):
+            self.parent = None
+            self.left = None
+            self.right = None
+            self.value = None
 
     def __init__(self):
-        pass
+        self.root = None
+        self.last = None
 
     def pop(self):
-        """ Returns and removes the minimum value from the heap.  If the heap is empty, return None.
-            Must be implemented with no worse than O(log n) time complexity. """
-        return None
+        """
+        Returns and removes the minimum value from the heap.
+        If the heap is empty, returns None.
+        """
+        if self.root == None: # ie heap is empty
+            return None
+        else:
+            output = self.root.value
+
+            # TODO: remove root value, replace with last value, and bubble down
+
+            return output
 
     def peek(self):
-        """ Return but do not remove the minimum value of the heap.  If the heap is empty, return None.
-            Must be implemented with no worse than O(1) time complexity. """
-        return None
+        """
+        Returns but does not remove the minimum value of the heap.
+        If the heap is empty, returns None.
+        """
+        if self.root == None: # empty heap
+            return None
+        
+        else:
+            return self.root.value
 
     def insert(self, data):
-        """ Insert the provided data onto the heap.  Should support integers and strings at least.
-            Must be implemented with no worse than O(log n) time complexity. """
+        """
+        Inserts the provided value onto the heap.
+        Raises a ValueError for types other than int, float, and str.
+        """
         return None
 
     def clear(self):
-        """ Removes all elements from the heap.  Must be implemented in no worse than O(1) time. """
+        """ Removes all elements from the heap."""
+        self.root = None
+        self.last = None
         return None
