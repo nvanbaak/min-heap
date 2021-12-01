f323356b5d264121e581f0625b362f6654d82434
Nik Van Baak <nvanbaak@gmail.com>
2021-12-01 12:19:51 -0800

    starter code

---
+++ MinHeap.py
@@ -0,0 +1,23 @@
+class MinHeap():
+
+    def __init__(self):
+        pass
+
+    def pop(self):
+        """ Returns and removes the minimum value from the heap.  If the heap is empty, return None.
+            Must be implemented with no worse than O(log n) time complexity. """
+        return None
+
+    def peek(self):
+        """ Return but do not remove the minimum value of the heap.  If the heap is empty, return None.
+            Must be implemented with no worse than O(1) time complexity. """
+        return None
+
+    def insert(self, data):
+        """ Insert the provided data onto the heap.  Should support integers and strings at least.
+            Must be implemented with no worse than O(log n) time complexity. """
+        return None
+
+    def clear(self):
+        """ Removes all elements from the heap.  Must be implemented in no worse than O(1) time. """
+        return None
