610013ed5342861d2d608976966ad05093b28571
Nik Van Baak <nvanbaak@gmail.com>
2021-12-01 14:40:42 -0800

    implemented logic for finding next open slot

--- MinHeap.py
+++ MinHeap.py
@@ -13,10 +13,22 @@ class MinHeap():
             self.value = None
             self.is_left = None
 
+        def __eq__(self, other_node):
+            """
+            Equality operator
+            """
+            return self.value == other_node.value
+
+        def __lt__(self, other_node):
+            """
+            Less-than operator
+            """
+            return self.value < other_node.value
+
+
     def __init__(self):
         self.root = None
         self.last = None
-        self.next_open = None
 
     def pop(self):
         """
@@ -46,31 +58,61 @@ class MinHeap():
     def insert(self, data):
         """
         Inserts the provided value onto the heap.
-        Raises a ValueError for types other than int, float, and str.
+        Raises a TypeError for types other than int, float, and str.
         """
         # type checking
         if not (isinstance(data, int) 
                 or isinstance(data, float)
                 or isinstance(data, str)):
-            raise ValueError("This MinHeap only supports int, float, and str.")
+            raise TypeError("This MinHeap only supports int, float, and str.")
 
-        if self.root == None: # trivial case, empty heap
-            # store data in root, set as last inserted
+        # easy case: empty heap
+        if self.root == None:
             n = self.HeapNode()
             n.value = data
             self.root = n
             self.last = n
-
-            # create empty node for next_open
-            next_node = self.HeapNode()
-            n.left = next_node
-            next_node.is_left = True
-            self.next_open = next_node
             return
 
-        current = self.root
-        while current:
-            return None
+        #######
+        # Using the last inserted node, we find the next open position for insertion
+        #######
+
+        # First, make a new node with the data
+        n = self.HeapNode()
+        n.value = data
+
+        # if the recent-most child is on the left, the right child is open
+        if self.last.is_left:
+            self.last.parent.right = n
+            n.is_left = False
+            self.last = n
+
+        else: # if the recent-most child is right, we traverse
+            node_pointer = self.last
+
+            # keep going up until we find root or a node that's the left child
+            while not node_pointer.is_left:
+                if node_pointer.parent is not None:
+                    node_pointer = node_pointer.parent
+                else: # pointer's at root, nowhere to go
+                    break
+
+            # if we hit a left child before root, move pointer to its right sibling
+            if node_pointer.parent is not None:
+                node_pointer = node_pointer.parent.right
+
+            # The next open slot should now be a straight shot down the left children
+            while node_pointer.left is not None:
+                node_pointer = node_pointer.left
+
+            # Insert new node as left child
+            node_pointer.left = n
+            n.parent = node_pointer
+            n.is_left = True
+            self.last = n
+
+        return None
 
     def clear(self):
         """ Removes all elements from the heap."""
@@ -78,3 +120,9 @@ class MinHeap():
         self.last = None
         self.next_open = None
         return None
+
+    def set_next_open_node(self):
+        """
+        Given a node in the tree, 
+        """
+        pass
