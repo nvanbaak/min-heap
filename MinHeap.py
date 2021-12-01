553996a1950e83272f4ec55ec5fbd68776266171
Nik Van Baak <nvanbaak@gmail.com>
2021-12-01 15:04:39 -0800

    insert now bubbles up the inserted value

--- MinHeap.py
+++ MinHeap.py
@@ -13,6 +13,24 @@ class MinHeap():
             self.value = None
             self.is_left = None
 
+        def append_right(self, node):
+            """
+            Appends a given node to the right with appropriate linking.
+            """
+            self.right = node
+            node.parent = self
+            node.is_left = False
+            return
+
+        def append_left(self, node):
+            """
+            Appends a given node to the left with appropriate linking.
+            """
+            self.left = node
+            node.parent = self
+            node.is_left = True
+            return
+
         def __eq__(self, other_node):
             """
             Equality operator
@@ -84,11 +102,10 @@ class MinHeap():
 
         # if the recent-most child is on the left, the right child is open
         if self.last.is_left:
-            self.last.parent.right = n
-            n.is_left = False
+            self.last.parent.append_right(n)
             self.last = n
 
-        else: # if the recent-most child is right, we traverse
+        else: # if the recent-most child is right, we traverse to find the next slot
             node_pointer = self.last
 
             # keep going up until we find root or a node that's the left child
@@ -102,15 +119,17 @@ class MinHeap():
             if node_pointer.parent is not None:
                 node_pointer = node_pointer.parent.right
 
-            # The next open slot should now be a straight shot down the left children
+            # The next open position should now be a straight shot down the left children
+            # from our current position.
             while node_pointer.left is not None:
                 node_pointer = node_pointer.left
 
             # Insert new node as left child
-            node_pointer.left = n
-            n.parent = node_pointer
-            n.is_left = True
+            node_pointer.append_left(n)
             self.last = n
+
+        # now that the new node is in self.last, bubble up its value
+        self.bubble_up_last()
 
         return None
 
@@ -118,11 +137,25 @@ class MinHeap():
         """ Removes all elements from the heap."""
         self.root = None
         self.last = None
-        self.next_open = None
         return None
 
-    def set_next_open_node(self):
+    def bubble_up_last(self):
         """
-        Given a node in the tree, 
+        Called after insertion operation.
+        Bubbles up the value in self.last until it's larger than its parent node.
         """
-        pass
+
+        node_pointer = self.last
+
+        # repeat until we hit root
+        while node_pointer.parent is not None:
+
+            # swap values if we're smaller, then move up one level
+            if node_pointer < node_pointer.parent:
+                node_pointer.value, node_pointer.parent.value = node_pointer.parent.value, node_pointer.value
+                node_pointer = node_pointer.parent
+
+            else: # if the parent is smaller, we're done
+                break
+
+        return None
